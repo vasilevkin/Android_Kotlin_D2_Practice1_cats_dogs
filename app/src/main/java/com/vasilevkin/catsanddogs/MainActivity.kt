4ab85ce4ff4dbac24b835cf5cc3c695e7cf91738
Sergey Vasilevkin <svasilev@mera.ru>
2020-01-24 23:17:52 +0300

    Add recyclerview.

--- app/src/main/java/com/vasilevkin/catsanddogs/MainActivity.kt
+++ app/src/main/java/com/vasilevkin/catsanddogs/MainActivity.kt
@@ -2,11 +2,36 @@ package com.vasilevkin.catsanddogs
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.DividerItemDecoration
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var recyclerView: RecyclerView
+    private lateinit var layoutManager: LinearLayoutManager
+    private lateinit var adapter: CatsAndDogsAdapter
+
+    private lateinit var catsAndDogs: Array<String>
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        catsAndDogs = arrayOf("cat", "dog")
+
+        // set up the RecyclerView
+        recyclerView = findViewById(R.id.cats_and_dogs_recyclerview)
+        layoutManager = LinearLayoutManager(this)
+        recyclerView.layoutManager = layoutManager
+
+        adapter = CatsAndDogsAdapter(catsAndDogs)
+        recyclerView.adapter = adapter
+
+        val dividerItemDecoration = DividerItemDecoration(
+            recyclerView.context,
+            layoutManager.orientation
+        )
+        recyclerView.addItemDecoration(dividerItemDecoration)
     }
 }
