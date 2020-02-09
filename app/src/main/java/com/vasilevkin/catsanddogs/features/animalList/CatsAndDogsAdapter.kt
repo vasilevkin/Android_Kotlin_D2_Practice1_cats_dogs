fe8c8ffa7759606bfd2bc0a74c93965bad67af5d
Sergey Vasilevkin <svasilev@mera.ru>
2020-02-09 12:03:18 +0300

    Update recyclerView, item and adapter.

--- app/src/main/java/com/vasilevkin/catsanddogs/features/animalList/CatsAndDogsAdapter.kt
+++ app/src/main/java/com/vasilevkin/catsanddogs/features/animalList/CatsAndDogsAdapter.kt
@@ -5,43 +5,68 @@ import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
 import com.vasilevkin.catsanddogs.R
-import com.vasilevkin.catsanddogs.models.localModels.Animal
-import com.vasilevkin.catsanddogs.utils.downloadImageInView
+import com.vasilevkin.catsanddogs.models.networkModels.CatRemoteModel
+import kotlinx.android.synthetic.main.long_horizontal_item.view.*
 
 
-class CatsAndDogsAdapter
-internal constructor(context: Context, private val catsAndDogsDataset: List<Animal>) :
-    RecyclerView.Adapter<CatsAndDogsHolder>() {
+class CatAdapter(private val breedList: List<CatRemoteModel>, private val context: Context) :
+    RecyclerView.Adapter<ViewHolder>() {
 
-    private val inflater: LayoutInflater
-    private val con = context
-
-    init {
-        this.inflater = LayoutInflater.from(context)
+    override fun onCreateViewHolder(p0: ViewGroup, p1: Int): ViewHolder {
+        return ViewHolder(
+            LayoutInflater.from(context).inflate(
+                R.layout.long_horizontal_item,
+                p0,
+                false
+            )
+        )
     }
 
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): CatsAndDogsHolder {
-        val view = inflater.inflate(R.layout.long_horizontal_item, parent, false)
-        return CatsAndDogsHolder(view)
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        holder.itemView.title_text_view.text = breedList.get(position).name
+        holder.itemView.subtitle_text_view.text = breedList.get(position).origin
     }
 
-    // binds the data to the TextView in each row
-    override fun onBindViewHolder(holder: CatsAndDogsHolder, position: Int) {
-        val title = catsAndDogsDataset[position].title
-//        val description = catsAndDogsDataset[position].description
-        val imageUrl = catsAndDogsDataset[position].imageUrl
-
-        holder.titleTextView.text = title
-//        holder.descriptionTextView.text = description
-
-        downloadImageInView(con, holder.detailsImage, "https://images.dog.ceo/breeds/terrier-wheaten/n02098105_1777.jpg")
-//        Glide.with(con).load("https://images.dog.ceo/breeds/terrier-wheaten/n02098105_1777.jpg").into(holder.detailsImage)
-
-//        Picasso.get().load("https://images.dog.ceo/breeds/terrier-wheaten/n02098105_1777.jpg").into(holder.detailsImage)
-    }
-
-    // Return the size of your dataset (invoked by the layout manager)
-    override fun getItemCount() = catsAndDogsDataset.size
-
-
+    override fun getItemCount() = breedList.size
 }
+
+
+//class CatsAndDogsAdapter
+//internal constructor(context: Context, private val catsAndDogsDataset: List<Animal>) :
+//    RecyclerView.Adapter<CatsAndDogsHolder>() {
+//
+//    private val inflater: LayoutInflater
+//    private val con = context
+//
+//    init {
+//        this.inflater = LayoutInflater.from(context)
+//    }
+//
+//    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): CatsAndDogsHolder {
+//        val view = inflater.inflate(R.layout.long_horizontal_item, parent, false)
+//        return CatsAndDogsHolder(view)
+//    }
+//
+//    // binds the data to the TextView in each row
+//    override fun onBindViewHolder(holder: CatsAndDogsHolder, position: Int) {
+//        val title = catsAndDogsDataset[position].title
+////        val description = catsAndDogsDataset[position].description
+////        val imageUrl = catsAndDogsDataset[position].imageUrl
+//
+//        holder.titleTextView.text = title
+////        holder.descriptionTextView.text = description
+//
+//        downloadImageInView(con, holder.detailsImage, "https://images.dog.ceo/breeds/terrier-wheaten/n02098105_1777.jpg")
+////        Glide.with(con).load("https://images.dog.ceo/breeds/terrier-wheaten/n02098105_1777.jpg").into(holder.detailsImage)
+//
+////        Picasso.get().load("https://images.dog.ceo/breeds/terrier-wheaten/n02098105_1777.jpg").into(holder.detailsImage)
+//    }
+//
+//    // Return the size of your dataset (invoked by the layout manager)
+//    override fun getItemCount() = catsAndDogsDataset.size
+//
+//
+//}
+
+
+
--- app/src/main/java/com/vasilevkin/catsanddogs/views/MainActivity.kt
+++ app/src/main/java/com/vasilevkin/catsanddogs/views/MainActivity.kt
@@ -1,39 +1,42 @@
 package com.vasilevkin.catsanddogs.views
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Log
-import androidx.recyclerview.widget.DividerItemDecoration
+import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.LinearLayoutManager
-import androidx.recyclerview.widget.RecyclerView
-import com.facebook.stetho.okhttp3.StethoInterceptor
-import com.jakewharton.retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory
 import com.vasilevkin.catsanddogs.R
-import com.vasilevkin.catsanddogs.features.animalList.CatsAndDogsAdapter
-import com.vasilevkin.catsanddogs.models.localModels.Animal
-import com.vasilevkin.catsanddogs.network.NetworkDataService
-import com.vasilevkin.catsanddogs.network.ServiceGetter
-import com.vasilevkin.catsanddogs.repository.AnimalRepository
-import com.vasilevkin.catsanddogs.repository.dataSource.CloudDataSource
+import com.vasilevkin.catsanddogs.features.animalList.CatAdapter
+import com.vasilevkin.catsanddogs.models.networkModels.CatRemoteModel
+import com.vasilevkin.catsanddogs.utils.getDataServiceCommon
 import io.reactivex.android.schedulers.AndroidSchedulers
-import io.reactivex.schedulers.Schedulers
-import okhttp3.OkHttpClient
-import retrofit2.Retrofit
-import retrofit2.converter.gson.GsonConverterFactory
+import io.reactivex.internal.schedulers.IoScheduler
+import kotlinx.android.synthetic.main.activity_main.*
+
 
 class MainActivity : AppCompatActivity() {
 
-    private lateinit var recyclerView: RecyclerView
-    private lateinit var layoutManager: LinearLayoutManager
-    private lateinit var adapter: CatsAndDogsAdapter
-
-    private lateinit var catsAndDogs: List<Animal>
+    private var cats: List<CatRemoteModel> = emptyList()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        catsAndDogs = ArrayList<Animal>()
+        catList.layoutManager = LinearLayoutManager(this)
+
+        val catsApi = getDataServiceCommon()
+        val response = catsApi.getAllBreeds()
+
+        response
+            .observeOn(AndroidSchedulers.mainThread())
+            .subscribeOn(IoScheduler())
+            .subscribe({ cats ->
+                this.cats = cats
+                catList.adapter = CatAdapter(cats, this)
+            })
+    }
+}
+
+
+//        catsAndDogs = ArrayList<Animal>()
 
 
 //        val downloads = AnimalRepository(CloudDataSource()).getAllAnimals()
@@ -62,58 +65,56 @@ class MainActivity : AppCompatActivity() {
 //            )
 //        )
 
-        // set up the RecyclerView
-        recyclerView = findViewById(R.id.cats_and_dogs_recyclerview)
-        layoutManager = LinearLayoutManager(this)
-        recyclerView.layoutManager = layoutManager
+// set up the RecyclerView
+//        recyclerView = findViewById(R.id.cats_and_dogs_recyclerview)
+//        layoutManager = LinearLayoutManager(this)
+//        recyclerView.layoutManager = layoutManager
+//
+//        adapter =
+//            CatsAndDogsAdapter(this, catsAndDogs)
+//        recyclerView.adapter = adapter
+//
+//        val dividerItemDecoration = DividerItemDecoration(
+//            recyclerView.context,
+//            layoutManager.orientation
+//        )
+//        recyclerView.addItemDecoration(dividerItemDecoration)
+//    }
 
-        adapter =
-            CatsAndDogsAdapter(this, catsAndDogs)
-        recyclerView.adapter = adapter
-
-        val dividerItemDecoration = DividerItemDecoration(
-            recyclerView.context,
-            layoutManager.orientation
-        )
-        recyclerView.addItemDecoration(dividerItemDecoration)
-    }
-
-    override fun onResume() {
-        super.onResume()
-
-        val okCLientInstance1: OkHttpClient = OkHttpClient.Builder()
-            .addNetworkInterceptor(StethoInterceptor())
-            .build()
-
-        val retrofitInstance1: NetworkDataService = Retrofit.Builder()
-//            .baseUrl("https://api.myjson.com/")
-            .baseUrl("https://dog.ceo/")
-            .addCallAdapterFactory(RxJava2CallAdapterFactory.create())
-//            .client(okCLientInstance1)
-            .addConverterFactory(GsonConverterFactory.create())
-            .build()
-            .create(NetworkDataService::class.java)
-
-        val catsAndDogs1 = retrofitInstance1.getAnimals1()
+//    override fun onResume() {
+//        super.onResume()
+//
+//        val okCLientInstance1: OkHttpClient = OkHttpClient.Builder()
+//            .addNetworkInterceptor(StethoInterceptor())
+//            .build()
+//
+//        val retrofitInstance1: NetworkDataService = Retrofit.Builder()
+////            .baseUrl("https://api.myjson.com/")
+//            .baseUrl("https://dog.ceo/")
+//            .addCallAdapterFactory(RxJava2CallAdapterFactory.create())
+////            .client(okCLientInstance1)
+//            .addConverterFactory(GsonConverterFactory.create())
+//            .build()
+//            .create(NetworkDataService::class.java)
+//
 //        val catsAndDogs1 = retrofitInstance1.getAnimals1()
-
-        Log.d("a1:7", "catsAndDogs1 = $catsAndDogs1")
-
-        catsAndDogs1
-            .subscribeOn(Schedulers.single())
-            .observeOn(AndroidSchedulers.mainThread())
-            .subscribe({ t ->
-
-                Log.d("a1:7", "ResponseBody = ${t.string()}")
-
-//                catsAndDogs = t as ArrayList<Animal>
-//            Log.d("a1:7", "ccatsAndDogs = $catsAndDogs")
-
-            },
-                { error -> Log.e("a1:7", "{$error.message}")}
-            )
-
-    }
-
-
-}
+////        val catsAndDogs1 = retrofitInstance1.getAnimals1()
+//
+//        Log.d("a1:7", "catsAndDogs1 = $catsAndDogs1")
+//
+//        catsAndDogs1
+//            .subscribeOn(Schedulers.single())
+//            .observeOn(AndroidSchedulers.mainThread())
+//            .subscribe({ t ->
+//
+//                Log.d("a1:7", "ResponseBody = ${t.string()}")
+//
+////                catsAndDogs = t as ArrayList<Animal>
+////            Log.d("a1:7", "ccatsAndDogs = $catsAndDogs")
+//
+//            },
+//                { error -> Log.e("a1:7", "{$error.message}")}
+//            )
+//
+//    }
+//}
