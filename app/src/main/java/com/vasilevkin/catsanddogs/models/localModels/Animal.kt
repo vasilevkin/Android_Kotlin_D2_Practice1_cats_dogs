fb228acd691260a88f635114a0404c576477873a
Sergey Vasilevkin <svasilevkin@gmail.com>
2020-02-21 12:29:53 +0300

    MVP: Implement repository.

--- app/src/main/java/com/vasilevkin/catsanddogs/models/localModels/Animal.kt
+++ app/src/main/java/com/vasilevkin/catsanddogs/models/localModels/Animal.kt
@@ -2,5 +2,6 @@ package com.vasilevkin.catsanddogs.models.localModels
 
 data class Animal(
     val title: String? = null,
+    val subtitle: String? = null,
     val imageUrl: String? = null
 )
--- app/src/main/java/com/vasilevkin/catsanddogs/repository/AnimalRepository.kt
+++ app/src/main/java/com/vasilevkin/catsanddogs/repository/AnimalRepository.kt
@@ -1,7 +1,7 @@
 package com.vasilevkin.catsanddogs.repository
 
 import com.vasilevkin.catsanddogs.models.localModels.Animal
-import com.vasilevkin.catsanddogs.repository.dataSource.ICloudDataSource
+import com.vasilevkin.catsanddogs.utils.getDataServiceCommon
 import io.reactivex.Single
 
 
@@ -9,15 +9,121 @@ class AnimalRepository(
 //    private val cloudDataSource: ICloudDataSource
 ) : IAnimalRepository {
 
-    override fun getAllAnimals(): Single<List<Animal>> {
-        return Single.just(listOf(Animal()))
-//        return cloudDataSource.getAnimals().map {
-//                it.map {
-//                    Animal(it.photo?.get(0)?.orEmpty())
-//                }
-//            }
-        }
 
+    override fun getAllAnimals(): Single<List<Animal>> {
+
+        val catsApi = getDataServiceCommon()
+
+        val response = catsApi.getBreedWithImages()
+//        val response = catsApi.getAllBreeds()
+
+        return response
+            .map { catsFromNetwork ->
+                catsFromNetwork.map {
+                    Animal(
+                        it.breeds?.get(0)?.name,
+                        it.breeds?.get(0)?.origin,
+                        it.imageUrl!!
+                    )
+                }
+            }
+    }
+}
+
+
+
+
+
+
+                    //            .subscribe { catsFromNetwork ->
+                //                this.cats = cats
+                //                catList.adapter = CatAdapter(cats, this)
+
+
+
+
+//                val objects = ArrayList<Animal>(20)
+////                val random = Random()
+//                for (i in 0 until catsFromNetwork.size) {
+//                    val item: Animal
+//
+//                    item = Animal(
+//                        catsFromNetwork[i].breeds?.get(0)?.name!!,
+//                        catsFromNetwork[i].breeds?.get(0)?.origin!!,
+//                        catsFromNetwork[i].imageUrl!!
+//                    )
+//
+//                    objects.add(item)
+//
+////                    val type = random.nextInt(3)
+////                    if (type == 0) {
+//////                    item = SquareCatLocalModel(
+////                        item = LongHorizontalCatLocalModel(
+////                            view as Activity,
+////                            cats[i].breeds?.get(0)?.name!!,
+////                            cats[i].breeds?.get(0)?.origin!!,
+////                            cats[i].imageUrl!!
+////                        )
+////                    } else if (type == 1) {
+////                        item = BigViewpagerLocalModel(
+////                            view as Activity,
+////                            cats[i].breeds?.get(0)?.name!!,
+////                            cats[i].breeds?.get(0)?.origin!!,
+////                            cats[i].imageUrl!!
+////                        )
+////
+////                    } else {
+////                        item = SquareCatLocalModel(
+//////                    item = LongHorizontalCatLocalModel(
+////                            view as Activity,
+////                            cats[i].breeds?.get(0)?.name!!,
+////                            cats[i].breeds?.get(0)?.origin!!,
+////                            cats[i].imageUrl!!
+////                        )
+////                    }
+////                    objects.add(item)
+//                }
+////                return objects
+//
+////                view?.displayAnimals(objects)
+//
+//
+////                animals =  Single.just(objects)
+//
+//            }
+//
+//
+//
+//return animals
+//
+////        return Single.just(listOf(Animal()))
+////        return cloudDataSource.getAnimals().map {
+////                it.map {
+////                    Animal(it.photo?.get(0)?.orEmpty())
+////                }
+////            }
+//
+//
+//
+//
+//
+//
+//    }
+//
+//
+//}
+
+
+
+
+//    override fun getAllAnimals(): Single<List<Animal>> {
+//        return Single.just(listOf(Animal()))
+////        return cloudDataSource.getAnimals().map {
+////                it.map {
+////                    Animal(it.photo?.get(0)?.orEmpty())
+////                }
+////            }
+//        }
 
 //    fun getAllAnimals(): Single<List<Animal>> {
 //        return Single.just(arrayListOf(
@@ -46,4 +152,3 @@ class AnimalRepository(
 //        ))
 //    }
 
-}
